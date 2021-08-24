c0a342c3cf22b967c0e7fe317059ae3734925555
Guigalaverna <guigalaverna@gmail.com>
2021-08-24 00:53:07 -0300

    feat: customize theme

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,6 +1,40 @@
 import { Theme, extendTheme } from "@chakra-ui/react";
 
 export const theme: Theme = extendTheme({
+  fonts: {
+    body: 'Roboto, system-ui, sans-serif',
+    heading: 'Roboto, system-ui, sans-serif',
+    mono: "Menlo, monospace",
+  },
+
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    bold: 700
+  },
+
+  raddi: {
+    sm: '5px',
+    md: '8px'
+  },
+
+  fontSizes: {
+    '6xl': '54px'
+  },
+
+  colors: {
+    purple: {
+      500: '#8257e5'
+    },
+
+    gray: {
+      300: '#e1e1e6',
+      600: '#29292e',
+      700: '#202024',
+      800: "#121214"
+    }
+  },
+
   styles: {
     global: {
       body: {
