7dc087fa2ff9825ab227c421b7d96b3d1cc1b341
Guigalaverna <guigalaverna@gmail.com>
2021-08-24 02:52:21 -0300

    feat: create notes list

---
+++ src/components/Dashboard/Course/Content/Notes/index.tsx
@@ -0,0 +1,43 @@
+import {Avatar, Box, Divider, Flex, Text} from "@chakra-ui/react";
+
+export function Questions() {
+  return (
+    <Flex w="100%">
+      <Flex
+        w="100%"
+        align="center"
+        justify="space-evenly"
+        bg="gray.700"
+        px="10"
+        py="8"
+        borderRadius="md"
+      >
+        <Flex>
+          <Avatar name="Guilherme Galaverna" />
+          <Box ml="5">
+            <Text>Guilherme Galaverna</Text>
+            <Text>24/08/2021</Text>
+          </Box>
+          <Divider orientation="vertical" mx="4" />
+        </Flex>
+        <Flex maxW="520px" direction="column">
+          <Text as="strong" mb="2">
+            Como ligo o PC 😁
+          </Text>
+          <Text
+            as="p"
+            overflow="hidden"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            color="gray.500"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos porro
+            consequuntur possimus nostrum. Necessitatibus doloribus, quisquam
+            suscipit, esse molestias exercitationem illo, inventore deserunt
+            quasi natus debitis eligendi sint quo tenetur!
+          </Text>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
